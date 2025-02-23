12d0f93ca4a0916202b714965ac5c1def977a845
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-23 06:51:54 -0800

    update views and template
    redefined index view to pass arguements to template
    added bio_text in about section in index

--- myresume/resume/views.py
+++ myresume/resume/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from resume.models import *
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    degrees = Education.objects.all()
+    skills = Skill.objects.all()
+    projects = Project.objects.all()
+    about = Bio.objects.all()[0]
+    languages = Language.objects.all()
+    context = {
+        'degrees' : degrees,
+        'skills' : skills,
+        'projects' : projects,
+        'about' : about,
+        'languages' : languages,
+    }
+    return render(request, 'index.html', context)
