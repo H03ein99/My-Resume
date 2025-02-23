4ac5b7de99c526dbbb991b08b0579c60d90ba607
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-23 04:00:01 -0800

    register models
    made migrations
    registered models at admin file in resume app

--- myresume/resume/admin.py
+++ myresume/resume/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from resume.models import Education, Skill, Project, Bio
 
 # Register your models here.
+admin.site.register(Education)
+admin.site.register(Skill)
+admin.site.register(Project)
+admin.site.register(Bio)
