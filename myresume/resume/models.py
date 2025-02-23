f4acbd84ae3e7bc6d52536658dcbac42c7071b18
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-23 03:55:52 -0800

    added models
    created Education, Skill, Project and Bio model at resume.models

--- myresume/resume/models.py
+++ myresume/resume/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Education(models.Model):
+    degree = models.CharField(max_length=100)
+    institution = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.degree} - {self.institution}"
+    
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    proficiency = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name} ({self.proficiency})"
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    discription = models.TextField()
+    github_url = models.URLField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Bio(models.Model):
+    bio_text = models.TextField()
+
+    def __str__(self):
+        return "Bio Information"
