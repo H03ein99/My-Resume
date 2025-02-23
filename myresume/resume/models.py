c72143cb009279310a3f06a84a6a4e5bc3a9e280
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-23 04:11:30 -0800

    modify previous models
    in Education added start and end date field and migrated

--- myresume/resume/models.py
+++ myresume/resume/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Education(models.Model):
     degree = models.CharField(max_length=100)
     institution = models.CharField(max_length=100)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.degree} - {self.institution}"
