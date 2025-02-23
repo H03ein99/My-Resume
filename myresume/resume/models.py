3e723df43ecae0ab0f1e4dece34c1698cd67e71a
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-23 08:27:22 -0800

    Update models.py
    add major in education model

--- myresume/resume/models.py
+++ myresume/resume/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Education(models.Model):
     degree = models.CharField(max_length=100)
+    major = models.CharField(max_length=100, blank=True, null=True)
     institution = models.CharField(max_length=100)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
