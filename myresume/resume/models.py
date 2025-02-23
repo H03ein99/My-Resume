6e6b53c84653be945365934d94eb7de2f16f7fe2
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-23 07:55:01 -0800

    modify profile
    added phone field

--- myresume/resume/models.py
+++ myresume/resume/models.py
@@ -71,6 +71,7 @@ class Profile(models.Model):
     degree = models.CharField(max_length=200)
     email = models.EmailField()
     about = models.TextField(null=True, blank=True)
+    phone = models.CharField(max_length=30, blank=True, null=True)
 
     def __str__(self):
         return f"{self.first_name} - {self.last_name}"
