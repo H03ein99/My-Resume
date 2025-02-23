340c287937e2482cf498bf1da57c6a7b32289756
Mohammad Hossein Ghorbani <hosseingh539@gmail.com>
2025-02-23 06:13:41 -0800

    Update admin.py
    Registered Language model

--- myresume/resume/admin.py
+++ myresume/resume/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from resume.models import Education, Skill, Project, Bio
+from resume.models import Education, Skill, Project, Bio, Language
 
 # Register your models here.
 admin.site.register(Education)
 admin.site.register(Skill)
 admin.site.register(Project)
 admin.site.register(Bio)
+admin.site.register(Language)
